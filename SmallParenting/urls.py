1e121db221ae33b4a0acade326d31983fff906b6
Proyecto Paillalef <alen.veas@gmail.com>
2023-09-20 00:03:18 -0300

    Mi cuarto commit

--- SmallParenting/urls.py
+++ SmallParenting/urls.py
@@ -5,6 +5,7 @@ from SmallParentingAPI.views import chat_SALUD_MENTAL_ESTUDIANTIL, chat_FALTA_DE
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views as auth_views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('registro/', registro, name='registro'),
@@ -30,9 +31,8 @@ urlpatterns = [
     path('historial/<int:perfil_hijo_id>/', ver_historial_perfil, name='ver_historial_perfil'),
 ]
 
+urlpatterns += staticfiles_urlpatterns()
 urlpatterns += [path('admin/', admin.site.urls)]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
-
-
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
